066097b9e2eec2201025d5f1cfca471112d5525d
TonyBarretor <tony.barretor@gmail.com>
2024-03-28 17:15:07 -0700

    fix: pycodestyle

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -5,16 +5,20 @@
 Module: 3-to_str
 """
 
+
 def to_str(n: float) -> str:
-
-
+    
+    
     """
     Returns the string representation of the given float.
 
     Args:
+
         n (float): The input float number.
 
     Returns:
+
         str: The string representation of the input number.
     """
+    
     return str(n)
