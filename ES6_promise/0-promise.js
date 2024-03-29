be72496292df96a07da722ad6976580c9a1dd1b0
TonyBarretor <tony.barretor@gmail.com>
2024-03-28 19:04:10 -0700

    feat: ES6promise task 0 to task9

--- ES6_promise/0-promise.js
+++
@@ -1,8 +0,0 @@
-export default function getResponseFromAPI() {
-    // Simulate an API call with a delay (Could be replace with actual API logic)
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({ data: 'Successfully retrieved data' });
-      }, 1000);
-    });
-  }
--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,5 +1,5 @@
 export default async function loadBalancer(chinaDownload, USDownload) {
-    return Promise.race([chinaDownload, USDownload]).then(
-      (firstResolved) => firstResolved,
-    );
-  }
+  return Promise.race([chinaDownload, USDownload]).then(
+    (firstResolved) => firstResolved,
+  );
+}
