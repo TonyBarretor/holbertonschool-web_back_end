81acfd1ccd5bf862dc86abfdbde7823e691f2cb5
TonyBarretor <tony.barretor@gmail.com>
2024-03-30 16:50:18 -0700

    fix: list in task 100-weak.js

--- ES6_data_manipulation/100-weak.js
+++ ES6_data_manipulation/100-weak.js
@@ -1,21 +1,23 @@
 // 100-weak.js
 
-// Export a const instance of WeakMap named weakMap
+// Export a const instance of WeakMap and name it weakMap
 export const weakMap = new WeakMap();
 
 // Export a new function named queryAPI
 export function queryAPI(endpoint) {
-    // Check if the endpoint exists in the weakMap, if not initialize its count to 0
-    if (!weakMap.has(endpoint)) {
-        weakMap.set(endpoint, 0);
-    }
+  // Track within the weakMap the number of times queryAPI is called for each endpoint.
+  // Check if the endpoint exists in the weakMap, if not initialize its count to 0
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
 
-    // Increment the count for the endpoint
-    const count = weakMap.get(endpoint) + 1;
-    weakMap.set(endpoint, count);
+  // Increment the count for the endpoint
+  const count = weakMap.get(endpoint) + 1;
+  weakMap.set(endpoint, count);
 
-    // Check if the number of queries is >= 5, if so, throw an error
-    if (count >= 5) {
-        throw new Error('Endpoint load is high');
-    }
+  // When the number of queries is >= 5 throw an error with the message Endpoint load is high
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
 }
+
